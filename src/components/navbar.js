91ed54d8aa007217eb838dfbb6d5e207f5714f92
Li-Kane <122249106+Li-Kane@users.noreply.github.com>
2024-06-04 10:40:42 -0700

    Navbar fixes
    fixed navbar so that main width is always correct

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -160,6 +160,7 @@ class MyNavbar extends HTMLElement {
 
         if (localStorage.getItem('navbarMinimized') === 'true') {
             this.classList.add('minimized');
+            this.updateMainWidth();
         }
 
         // Add resize event listener
@@ -175,7 +176,7 @@ class MyNavbar extends HTMLElement {
      * and adjusts the width of the 'main' element accordingly.
      */
     toggleMinimize() {
-        if(window.innerWidth < 800) return;
+        if(window.innerWidth < 850) return;
         this.classList.toggle('minimized');
         localStorage.setItem('navbarMinimized', this.classList.contains('minimized'));
         this.updateMainWidth();
@@ -186,7 +187,7 @@ class MyNavbar extends HTMLElement {
      * Updates the width of the main element based on the presence of the 'minimized' class.
      */
     updateMainWidth() {
-        if(this.classList.contains('minimized')) {
+        if(localStorage.getItem('navbarMinimized') === 'true'){
             document.querySelector('main').style.width = `calc(100vw - 100px)`;
         } else {
             document.querySelector('main').style.width = `80vw`;
@@ -201,14 +202,14 @@ class MyNavbar extends HTMLElement {
      * sets the width of the 'main' element to '80vw', and updates the display state if the 'navbarMinimized' flag is not set.
      */
     handleResize() {
-        if (window.innerWidth < 800) {
+        if (window.innerWidth < 850) {
             this.classList.add('minimized');
-            this.updateMainWidth();
+            document.querySelector('main').style.width = `calc(100vw - 100px)`;
         } 
         else {
             if (localStorage.getItem('navbarMinimized') !== 'true') {
                 this.classList.remove('minimized');
-                this.updateMainWidth();
+                document.querySelector('main').style.width = `80vw`;
             }
         }
     }
