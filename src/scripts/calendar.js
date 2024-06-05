8611677305fe16bd47bf0d49d18b7f93243969c4
sierra392 <ssm24816@gmail.com>
2024-06-05 10:09:20 -0700

    my current versiom of calendar backend (changing date header is working but display not fully yet)

--- src/scripts/calendar.js
+++ src/scripts/calendar.js
@@ -108,6 +108,8 @@ function showCalendar(month, year) {
 		}
 		tbl.appendChild(row);
 	}
+	let datesForWeek = getDatesForWeek();
+	changeDateHeader(datesForWeek);
 	datesIntoButtons();
 }
 
@@ -129,54 +131,255 @@ function datesIntoButtons(){
 				document.querySelector(".date-picker.selected").className = "date-picker";
 			}
 			dates[i].className = "date-picker selected";
-			changeDateHeader();
+			let datesForWeek = getDatesForWeek();
+			changeDateHeader(datesForWeek);
 			clearTasks();
-			addNewTasks();
+			addNewTasks(datesForWeek);
 		});
 	}
+}
+/**
+ * Function to add a date in the correct format to the date array that contains the dates of a given week to display
+ */
+function addDateArr(datesArr, year, month, day){
+	month = JSON.stringify(month);
+	year = JSON.stringify(year);
+	day = JSON.stringify(day);
+	let formatMonth = month < 10 ? 0 + month : month;
+	let formatDay = day < 10 ? 0 + day : day;
+	let formatDate = year + '/' + formatMonth + '/' + formatDay;
+	datesArr.push({
+		dayOfMonth: day,
+		date : formatDate
+	});
+}
+
+/**
+ * Function get the dates for a specific week to display
+ */
+function getDatesForWeek(){ 
+	let dateSelected = document.querySelector(".date-picker.selected");
+	let dofW = parseInt(dateSelected.dataset['day_of_week'], 10);
+	let offset = 7 - dofW;
+	let date = parseInt(dateSelected.dataset['date'], 10);
+	let monthSelected = parseInt(dateSelected.dataset['month'],10) - 1;
+	let yearSelected = parseInt(dateSelected.dataset['year'],10);
+	let datesReturned = [];
+	if(date < dofW){ //case where begin columns are blank
+		let numBlank = dofW - date;
+		let prevMonth = monthSelected === 0 ?
+		11 : monthSelected - 1;
+		let prevYear = monthSelected === 0 ?
+		yearSelected - 1 : yearSelected;
+		for(let i = numBlank; i > 0; i--){
+			let dateToAdd = daysInMonth(prevMonth, prevYear) - (i - 1);
+			addDateArr(datesReturned, prevYear, prevMonth, dateToAdd);
+		}
+		for(let i = 1; i <= 7 - numBlank; i++){
+			addDateArr(datesReturned, yearSelected, monthSelected, i);
+		}
+
+	} else{ //case where whole row is filled or last columns are empty
+		for(let i = 1; i <= dofW; i++){ //get dates previous to selected date in row
+			let dateToAdd = date - (dofW - i);
+			addDateArr(datesReturned, yearSelected, monthSelected, dateToAdd);
+		}
+		for(let i = dofW + 1; i <= 7; i++){
+			let dateToAdd = date + (i - dofW);
+			if((date + offset) > daysInMonth(monthSelected, yearSelected)){
+				dateToAdd = i - dofW;
+				yearSelected = monthSelected === 11 ? yearSelected + 1 : yearSelected;
+			}
+			addDateArr(datesReturned, yearSelected, monthSelected, dateToAdd);
+		}
+	}
+	return datesReturned;
 }
 /**
  * Function to change the date header to the selected day
  */
 
-function changeDateHeader() {
-	let dateHeader = document.getElementById("curr-day");
-	let date = document.querySelector(".date-picker.selected");
-	let dofW = date.dataset['day_of_week'];
-	switch (dofW) {
-		case 1:
-			dofW = "Sunday"; break;
-		case 2:
-			dofW = "Moday"; break;
-		case 3:
-			dofW = "Tuesday"; break;
-		case 4:
-			dofW = "Wednesday"; break;
-		case 5:
-			dofW = "Thursday"; break;
-		case 6:
-			dofW = "Friday"; break;
-		case 7:
-			dofW = "Saturday"; break;
+function changeDateHeader(datesArr){
+	let tableHeaders = document.querySelectorAll('.day-detail');
+	for(let i = 0; i < 7; i++){
+		let headerDate = tableHeaders[i+1].querySelectorAll('h2')[1];
+		headerDate.innerHTML = datesArr[i].dayOfMonth;
 	}
-  
-	dateHeader.innerHTML = dofW + ", " + date.dataset['month_name'] + " " + date.dataset['date'];
 }
 /**
  * Function to clear the tasks that are being displayed for a selected date
  */
 function clearTasks(){
-	allEvents = document.querySelectorAll(".events");
-	for(let i = 0; i < allEvents.length; i++){
-		//remove all tasks from each hour
+	let allTasks = document.querySelectorAll(".tasks");
+	let numTasks = allTasks.length;
+	for(let i = 0; i < numTasks; i++){
+		allTasks[i].remove();
 	}
 }
-/**
- * Function to display the tasks that correspond to a selected day in its appropriate time slot
- */
-function addNewTasks(){
 
+/**
+ * Function to get the tasks for a given week in the form of an array with seven elements,
+ * each element containing an array with tasks for the corresponding day of the week
+ */
+function getWeekTasks(datesArr){
+	// let taskMap = localStorage.getItem('tasklist');
+	// taskMap = new Map(JSON.parse(tasksMap));//see if this is how it is done (what if null)
+	// let tasksArr = [];
+	// for(let i = 0; i < 7; i++){
+	// 	tasksArr[i] = taskMap.get(datesArr[i].date) || []; //is this how you write empty array if null?
+	// }
+	let task1 = {date: "06/02/2024",
+		description: "",
+		endTime: "06:45",
+		id: 1258705,
+		priority: "low",
+		startTime: "04:00",
+		tags: [],
+		title: "Task 1",
+		color: "green"
+		};
+	let task2 = {date: "06/02/2024",
+		description: "",
+		endTime: "14:59",
+		id: 1258705,
+		priority: "low",
+		startTime: "12:00",
+		tags: [],
+		title: "Task 2",
+		color: "blue"
+		};
+	let task3 = {date: "06/04/2024",
+		description: "",
+		endTime: "18:59",
+		id: 1258705,
+		priority: "low",
+		startTime: "13:00",
+		tags: [],
+		title: "Task 1",
+		color: "red"
+		};
+	let task4 = {date: "06/07/2024",
+		description: "",
+		endTime: "17:30",
+		id: 1258705,
+		priority: "low",
+		startTime: "16:59",
+		tags: [],
+		title: "Task 1",
+		color: "purple"
+		};
+	let tasksArr = [[task1, task2], [task3], [], [], [task4], [], []];
+	return tasksArr;
 }
+
+/**
+ * Function to round a given time (in the format "00:00") to the nearest 30 minutes
+ */
+function roundTimeBy30(timeUnrounded){
+	let [hr, min] = timeUnrounded.split(":").map(part => parseInt(part, 10));
+	if( min < 15){
+		min = 0;
+	} else if( min <= 30){
+		min = 30;
+	} else if(min < 45){
+		min = 30
+	} else{
+		min = 0;
+		hr += 1;
+	}
+	return [hr, min];
+}
+
+/**
+ * Function to format the tasks for each day to be an array where each element is an object
+ * containg the task, rounded start time, rounded end time
+ */
+function roundedFormat(tasksForDay){
+	let roundedFormat = [];
+	for(let i = 0; i < tasksForDay.length; i++){
+		let singleTask = tasksForDay[i];
+		const timeStart = singleTask['startTime'];
+		let newStart = roundTimeBy30(timeStart);
+		const timeEnd = singleTask['endTime'];
+		let newEnd = roundTimeBy30(timeEnd);
+		let taskInfoObj = {
+			task: tasksForDay[i],
+			roundStart: newStart,
+			roundEnd: newEnd
+		}
+		roundedFormat.push(taskInfoObj);
+
+	}
+	return roundedFormat;
+}
+
+/**
+ * Function to get the row length in pixels for a given task that will be displayed
+ */
+function mathRowLength(singularTask){
+	const startHr = singularTask['roundStart'][0];
+	const startMin = singularTask['roundStart'][1];
+	const endHr = singularTask['roundEnd'][0];
+	const endMin = singularTask['roundEnd'][1];
+	const result = 2 * (endHr - startHr) + (Math.abs(startMin - endMin) / 30);//calculate number of rows
+	return result * 40; //multiply the result by 40 to get the exact number length of a task in pixels (one row is 40 pixels)
+}
+
+/**
+ * Function to compare if a given tasks is before another task (or has an earlier start time)
+ */
+function compareIsBefore(task1, task2){
+	const hour1 = task1['roundStart'][0];
+	const minute1 = task1['roundStart'][1];
+	const hour2 = task2['roundStart'][0];
+	const min2 = task2['roundStart'][1];
+	if (hour1 < hour2 || (hour1 === hour2 && minute1 < minute2)) {
+		return 1; //task 1 is before
+	} else if (hour1 > hour2 || (hour1 === hour2 && minute1 > minute2)) {
+		return -1; //task 2 is before
+	} else {
+		return 0; //they have same start time
+	}
+}
+
+/**
+ * Function to clear sort the tasks array by earliest start time
+ */
+function sortTasks(tasksForDay){ 
+	for(let i = 1; i < tasksForDay.length; i++){
+		let curr = tasksForDay[i];
+		let j = i -1;
+		while(j >= 0 && compareIsBefore(tasksForDay[j], curr) > 0){
+			tasksForDay[j + 1] = tasksForDay[j];
+			j--;
+		}
+		tasksForDay[j+1] = curr;
+	}
+	return tasksForDay;
+}
+/**
+ * Function to display tasks for a given week on the grid
+ */
+function addNewTasks(datesArr){
+	let tasks = getWeekTasks(datesArr);
+	//console.log(tasks);
+	for(let i = 0; i < tasks.length; i++){
+		let tasksForDay = tasks[i];
+		tasksForDay = roundedFormat(tasksForDay);
+		console.log(tasksForDay)
+		tasksForDay = sortTasks(tasksForDay);
+		for(let j = 0; j < tasksForDay.length; j++){
+			let taskLen = mathRowLength(tasksForDay[j]);
+			let hr = tasksForDay['hour'] < 10 ? "0" + JSON.stringify(tasksForDay['hour']) : JSON.stringify(tasksForDay['hour']);
+			let min = tasksForDay['hour'] === 0 ? "00" : "30";
+			let rowId = "r" + hr + min;
+			console.log(rowId);
+			displaytaskCalendar(rowId, i + 1, tasksForDay['task'], taskLen);
+		}
+
+	}
+}
+
 /**
  * Function to add upcoming tasks to the Upcoming Tasks section
  */
@@ -186,4 +389,54 @@ function populateUpcoming(){
 	//find tasks assigned to it 
 
 
+}
+
+/**
+ * Function to display a singular task to its correct location on the grid
+ */
+function displaytaskCalendar(rowId, col, task,len) {
+    const rCont = document.querySelector(".right-container");
+    let newElem = document.createElement("div");
+    newElem.className = "task";
+    newElem.style.position = "absolute";
+	try{
+		console.log(task);
+	}catch(error){
+		console.log("?")
+		console.error(error)
+	}
+    newElem.textContent = task['title']; 
+	newElem.style.backgroundColor = task['color'];
+	//newElem.style.width = dimensions[0]; //gets width of element
+	newElem.style.height = len; //gets height of element
+
+    rCont.appendChild(newElem);
+    console.log(newElem);
+    let row = document.getElementById(rowId);
+    let cells = row.querySelectorAll("td");
+    let cell = cells[col]; 
+
+    const rect = cell.getBoundingClientRect();
+    newElem.style.top = `${rect.top + window.scrollY}px`;
+    newElem.style.left = `${rect.left + window.scrollX}px`; 
+
+    // Update the position on scroll
+    rCont.addEventListener("scroll", () => {
+        const rect = cell.getBoundingClientRect();
+        newElem.style.top = `${rect.top + window.scrollY}px`;
+        newElem.style.left = `${rect.left + window.scrollX}px`; 
+    });
+	window.addEventListener("resize", () => {
+        const rect = cell.getBoundingClientRect();
+        newElem.style.top = `${rect.top + window.scrollY}px`;
+        newElem.style.left = `${rect.left + window.scrollX}px`; 
+    });
+	let minBar = document.querySelector('.minimized');
+	let sR = minBar.shadowRoot; //is there shadow root stuff to do???
+	let minButton = sR.getElementById('minimize-btn');
+	minButton.addEventListener("click", () => {
+        const rect = cell.getBoundingClientRect();
+        newElem.style.top = `${rect.top + window.scrollY}px`; 
+        newElem.style.left = `${rect.left + window.scrollX}px`; 
+    });
 }
