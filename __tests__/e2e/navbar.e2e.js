c1092cdcab847a315b8f04a6d1e87f9b94f82634
Li-Kane <122249106+Li-Kane@users.noreply.github.com>
2024-06-05 17:24:00 -0700

    e2e tests Navbar

---
+++ __tests__/e2e/navbar.e2e.js
@@ -0,0 +1,51 @@
+describe('e2e testing for navbar', () => {
+    beforeAll(async () => {
+        await page.goto('http://localhost:3000', {waitUntil: 'networkidle2'});
+    });
+    const delay = (time) => new Promise((resolve) => setTimeout(resolve, time));
+
+    it('Check the 6 Navbar Items', async () => {
+        console.log('Checking for 6 navbar options...');
+        const navItems = await page.evaluate(async () => {
+            const shadowHost = document.querySelector("#main-page > my-navbar")
+            if (!shadowHost) throw new Error('No shadow host found');
+            const items = shadowHost.shadowRoot.querySelectorAll(".nav-row");
+            return Array.from(items).map(e => e.innerHTML);
+        });
+        expect(navItems.length).toBe(6);
+        console.log('Checking that each navItem has the correct text and icon...');
+        const expectedText = ['Home', 'Calendar', 'Tasks', 'Entries', 'Minimize', 'Settings'];
+        for (let i = 0; i < navItems.length; i++) {
+            expect(navItems[i]).toContain('img');
+            expect(navItems[i]).toContain(expectedText[i]);
+        }
+    });
+
+    it('Test the Minimize Button', async () => {
+        const navbar = await page.$('my-navbar');
+        // Grab the shadowRoot of that element
+        const shadowRoot = await navbar.getProperty('shadowRoot');
+        const minimizeBtn = await page.evaluateHandle(root => root.querySelector('#minimize-btn'), shadowRoot);
+        const sidebarHandle = await page.evaluateHandle(root => root.querySelector('#sidebar'), shadowRoot);
+        const sidebarWidth = await page.evaluate(sidebar => {
+            const computedStyle = window.getComputedStyle(sidebar);
+            return computedStyle.width;
+        }, sidebarHandle);
+        // expect sidebarWidth to be greater than 150px
+        expect(parseInt(sidebarWidth)).toBeGreaterThan(150);
+        // if the classes of my-navbar contain minimized, isMinized = true
+        const className = await (await navbar.getProperty('className')).jsonValue();
+        expect (className.includes('minimized')).toBe(false);
+
+        await minimizeBtn.click();
+        await delay(1000);
+        const classNameAfterClick = await (await navbar.getProperty('className')).jsonValue();
+        expect (classNameAfterClick.includes('minimized')).toBe(true);
+        // Check the width of the sidebar after clicking the minimize button
+        const sidebarWidthAfterClick = await page.evaluate(sidebar => {
+            const computedStyle = window.getComputedStyle(sidebar);
+            return computedStyle.width;
+        }, sidebarHandle);
+        expect(sidebarWidthAfterClick).toBe('100px');
+    });
+});
